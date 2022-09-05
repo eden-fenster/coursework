550ba78bbb23ce687bffdafe2e1ba4332fe7118b
eden-fenster <eden.fenster@gmail.com>
2022-09-05 05:53:12 +0300

    New test.

---
+++ 2012_b/cross_arrays.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def cross_sort(arr: List[int]) -> List[int]:
+    arr.sort(reverse=False)
+    return arr
+
+
+def cross_search(arr: List[int], x: int) -> int:
+    for i in range(0, len(arr)):
+        if arr[i] is not x:
+            continue
+        return i
+    return -1
